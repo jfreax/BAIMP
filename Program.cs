cf3a6c4452daaa5d2c71da498d62f62b2cbceeb9
Jens Dieskau <jdieskau@ucc.uni-magdeburg.de>
2014-03-13 09:52:50 +0000

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace bachelorarbeit_implementierung
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
