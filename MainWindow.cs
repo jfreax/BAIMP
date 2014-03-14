8c1cf8037ba01fcd0ab1ba5d09013ac5a8334911
Jens Dieskau <jens@dieskau.pm>
2014-03-14 17:59:28 +0100

    SplitContainer anstatt Splitter

--- MainWindow.cs
+++ MainWindow.cs
@@ -6,86 +6,71 @@ namespace bachelorarbeit_implementierung
 {
 	public class MainWindow : Form
 	{
-		PictureBox box;
-		TreeView treeView1;
-		Splitter splitter;
+        // Container
+        SplitContainer splitFiletreePreview;
+        SplitContainer splitPreviewMetadata;
+        TabControl previewTabControl;
+
+        // Widgets
+        PictureBox box;
+        TreeView filetree;
+
 
 		public MainWindow (Bitmap bmp)
 		{
-//			Splitter splitter = new Splitter ();
-//			splitter.Anchor = AnchorStyles.Top | AnchorStyles.Left;
-//			splitter.Dock = DockStyle.Left;
-//
-//			//FlowLayoutPanel layout = new FlowLayoutPanel ();
-//			//layout.Size = new System.Drawing.Size(228, 20);
-//			//layout.TabIndex = 0;
-//			//layout.Anchor = AnchorStyles.Top | AnchorStyles.Left;
-//			//layout.Dock = DockStyle.Fill;
-//
-//			tree_view = new TreeView ();
-//			tree_view.Anchor = AnchorStyles.Top | AnchorStyles.Left;
-//			//tree_view.Dock = DockStyle.Fill;
-//			//tree_view.Height = Height;
-//
-//			//layout.Controls.Add (tree_view);
-//			//Controls.Add (tree_view);
+            // Split container
+            splitFiletreePreview = new SplitContainer();
+            splitFiletreePreview.Orientation = Orientation.Vertical;
+            splitFiletreePreview.BorderStyle = BorderStyle.FixedSingle;
+            splitFiletreePreview.Dock = DockStyle.Fill;
 
-			DockStyle teststyle = DockStyle.Left;
+            splitPreviewMetadata = new SplitContainer();
+            splitPreviewMetadata.Orientation = Orientation.Vertical;
+            splitPreviewMetadata.BorderStyle = BorderStyle.FixedSingle;
+            splitPreviewMetadata.Dock = DockStyle.Fill;
 
-//
-			box = new PictureBox ();
-			box.SizeMode = PictureBoxSizeMode.Zoom;
-			box.Image = bmp;
-			box.Dock = DockStyle.Fill;
-			//box.Click += new EventHandler (clickhandler);
-			box.MouseDown += new MouseEventHandler (mouseup);
+            // Vorschaufenster
+            previewTabControl = new TabControl();
+            previewTabControl.Dock = DockStyle.Fill;
+            //previewTabControl.Appearance = TabAppearance.FlatButtons;
+
+            box = new PictureBox();
+            box.SizeMode = PictureBoxSizeMode.Zoom;
+            box.Image = bmp;
+            box.Dock = DockStyle.Fill;
+
+            TabPage tabIntensity = new TabPage();
+            tabIntensity.Text = "Intensity";
+            tabIntensity.Controls.Add(box);
+
+            TabPage tabTopography = new TabPage();
+            tabTopography.Text = "Topography";
+            //tabTopography.Controls.Add(box);
+
+            TabPage tabColor = new TabPage();
+            tabColor.Text = "Color";
+            //tabColor.Controls.Add(box);
+
+            previewTabControl.TabPages.Add(tabIntensity);
+            previewTabControl.TabPages.Add(tabTopography);
+            previewTabControl.TabPages.Add(tabColor);
+            splitFiletreePreview.Panel2.Controls.Add(previewTabControl);
 
 
-			treeView1 = new TreeView ();
+            // Scanauswahl
+			filetree = new TreeView ();
+			filetree.Dock = DockStyle.Left;
+			filetree.BorderStyle = BorderStyle.Fixed3D;
+			filetree.Nodes.Add ("TreeView Node");
+            filetree.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
 
-			splitter = new Splitter ();
-			splitter.Dock = teststyle;
-			splitter.MinExtra = 0;
-			splitter.MinSize = 0;
-			splitter.BorderStyle = BorderStyle.Fixed3D;
-			splitter.BackColor = Color.Red;
-			splitter.SplitterMoving += new SplitterEventHandler(splithandler);
-			splitter.SplitterMoved += new SplitterEventHandler(splittermoved);
-			//splitter.MouseClick += new MouseEventHandler ();
-			//splitter.MouseUp += new MouseEventHandler(mouseup);
 
-			treeView1.Dock = teststyle;
-			treeView1.BorderStyle = BorderStyle.Fixed3D;
-			treeView1.Nodes.Add ("TreeView Node");
+            splitFiletreePreview.Panel1.Controls.Add(filetree);
+            splitFiletreePreview.Panel2.Controls.Add(splitPreviewMetadata);
+            Controls.Add(splitFiletreePreview);
 
-			Controls.AddRange (new Control [] { box, splitter, treeView1});
-
-			this.ClientSize = new Size(500, 500);
-			this.CreateControl();
+			//Controls.AddRange (new Control [] { box, splitter, treeView1});
 		}
-
-
-		public void splithandler(object sender, SplitterEventArgs e) {
-			Console.WriteLine("SplitterMoving: SplitPosition: {0} (Event: split: {1},{2} mouse: {3},{4})", ((Splitter)sender).SplitPosition, e.SplitX, e.SplitY, e.X, e.Y);
-		}
-
-		public void splittermoved(object sender, SplitterEventArgs e) {
-			Console.WriteLine("SplitterMoved: SplitPosition: {0} (Event: split: {1},{2} mouse: {3},{4})", ((Splitter)sender).SplitPosition, e.SplitX, e.SplitY, e.X, e.Y);
-		}
-
-		public void splitterClicked(object sender, MouseEventArgs e) {
-		}
-
-		public void mouseup(object sender, MouseEventArgs e) {
-			Console.WriteLine("Got mouseup event " + e.Location);
-		}
-
-		public void clickhandler(object sender, EventArgs e) {
-			Console.WriteLine (e.ToString());
-			splitter.SplitPosition = 150;
-		}
-
-
 	}
 }
 
