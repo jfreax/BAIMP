d27734613ee16191ebce8bacbe99d23cccc4b705
Jens Dieskau <jens@dieskau.pm>
2014-03-15 20:09:49 +0000

    Tab change event

--- ui/widget/Preview.cs
+++ ui/widget/Preview.cs
@@ -8,6 +8,9 @@ namespace bachelorarbeit_implementierung
 	public class Preview : Notebook
 	{
 		ScrollView[] tabs;
+		Image[] images;
+
+		Scan currentScan;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="bachelorarbeit_implementierung.Preview"/> class.
@@ -15,6 +18,8 @@ namespace bachelorarbeit_implementierung
 		public Preview ()
 		{
 			tabs = new ScrollView[(int)ScanType.Metadata];
+			images = new Image[(int)ScanType.Metadata];
+
 			for (int i = 0; i < (int)ScanType.Metadata; i++) {
 				ScanView img = new ScanView ();
 				tabs[i] = new ScrollView(img);
@@ -30,15 +35,40 @@ namespace bachelorarbeit_implementierung
 		/// </summary>
 		/// <param name="scan">Scan.</param>
 		public void ShowPreviewOf(Scan scan) {
-			MemoryStream memoryStream = new MemoryStream();
-			scan.GetAsBitmap(ScanType.Intensity)
-				.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
-			memoryStream.Position = 0;
+			this.currentScan = scan;
 
-			ScrollView tab = tabs [this.CurrentTabIndex];
-			ScanView scanView = (ScanView) tab.Content;
-			scanView.Image = Image.FromStream (memoryStream);
-			scanView.MouseScrolled += OnPreviewScroll;
+			for(int i = 0; i < images.Length; i++) {
+				if (images[i] != null) {
+					images[i].Dispose ();
+					images[i] = null;
+				}
+			}
+
+			this.LoadPreview ((ScanType) this.CurrentTabIndex);
+		}
+
+
+		/// <summary>
+		/// Loads an specific preview into appropriate tab.
+		/// </summary>
+		/// <param name="type">Type.</param>
+		private void LoadPreview(ScanType type) {
+			if (images [(int)type] == null && currentScan != null) {
+
+				MemoryStream memoryStream = new MemoryStream ();
+				currentScan.GetAsBitmap (type)
+					.Save (memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+				memoryStream.Position = 0;
+
+				ScrollView tab = tabs [(int)type];
+				ScanView scanView = (ScanView)tab.Content;
+
+				scanView.Image = Image.FromStream (memoryStream);
+				images [(int)type] = scanView.Image;
+
+				ResizeImageToFit (tab);
+				scanView.MouseScrolled += OnPreviewScroll;
+			}
 		}
 
 
@@ -47,17 +77,9 @@ namespace bachelorarbeit_implementierung
 		/// </summary>
 		/// <param name="view">View.</param>
 		private void InitializeEvents(ScrollView view) {
+
 			view.BoundsChanged += delegate(object sender, EventArgs e) {
-				ScrollView sv = (ScrollView) sender;
-				ImageView iv = (ImageView) sv.Content;
-
-				if(iv != null && iv.Image != null) {
-
-					double width = sv.VisibleRect.Width / iv.Image.Size.Width;
-					double height = sv.VisibleRect.Height / iv.Image.Size.Height;
-
-					iv.Image = iv.Image.Scale( Math.Min(width, height) );
-				}
+				ResizeImageToFit((ScrollView) sender);
 			};
 
 			view.ButtonPressed += delegate(object sender, ButtonEventArgs e)
@@ -139,6 +161,19 @@ namespace bachelorarbeit_implementierung
 		}
 
 
+		void ResizeImageToFit(ScrollView sv) {
+			ImageView iv = (ImageView) sv.Content;
+
+			if(iv != null && iv.Image != null) {
+
+				double width = sv.VisibleRect.Width / iv.Image.Size.Width;
+				double height = sv.VisibleRect.Height / iv.Image.Size.Height;
+
+				iv.Image = iv.Image.Scale( Math.Min(width, height) );
+			}
+		}
+
+
 		/// <summary>
 		/// 
 		/// </summary>
@@ -206,6 +241,17 @@ namespace bachelorarbeit_implementierung
 			}
 
 			sc.MouseMoved += MouseMovedGtk;
+		}
+
+
+		/// <summary>
+		/// Raises the current tab changed event.
+		/// </summary>
+		/// <param name="e">E.</param>
+		protected override void OnCurrentTabChanged (EventArgs e) {
+			base.OnCurrentTabChanged (e);
+
+			LoadPreview((ScanType)this.CurrentTabIndex);
 		}
 	}
 }
