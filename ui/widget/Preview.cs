ee306e5c8912e668c78ff25e619af32a6ef0341b
Jens Dieskau <jens@dieskau.pm>
2014-03-16 23:41:13 +0000

    Code restructure and performance

--- ui/widget/Preview.cs
+++ ui/widget/Preview.cs
@@ -6,18 +6,14 @@ using System.Threading;
 
 namespace bachelorarbeit_implementierung
 {
-	public class Preview : Notebook
+	public class Preview : VBox
 	{
-		ScrollView[] tabs;
+		public delegate void MyCallBack (ScanType type);
 
-		Thread imageLoaderThread;
-
+		ScrollView tab;
+		ScanView scanView;
+		Notebook notebook;
 		double imageScale = 1.0;
-		double vScroll = 0.0;
-		double hScroll = 0.0;
-
-        private object lock_image_loading = new object();
-
 		Scan currentScan;
 
 		/// <summary>
@@ -25,145 +21,106 @@ namespace bachelorarbeit_implementierung
 		/// </summary>
 		public Preview ()
 		{
-			tabs = new ScrollView[(int)ScanType.Metadata];
+			tab = new ScrollView ();
+			InitializeEvents (tab);
 
+			notebook = new Notebook ();
 			for (int i = 0; i < (int)ScanType.Metadata; i++) {
-				ScanView img = new ScanView ();
-				tabs[i] = new ScrollView(img);
-				tabs [i].BorderVisible = false;
-				InitializeEvents (tabs [i]);
-
-				Add (tabs [i], Enum.GetName(typeof(ScanType), i));
+				notebook.Add (new FrameBox(), Enum.GetName (typeof(ScanType), i));
 			}
-		}
 
+			notebook.CurrentTabChanged += delegate(object sender, EventArgs e) {
+				ScanType type = (ScanType)notebook.CurrentTabIndex;
+				ShowPreview (type);
+			};
+
+			this.PackStart (notebook, false, false);
+
+			tab.BorderVisible = false;
+			this.Margin = 0;
+			this.PackEnd (tab, true, true);
+		}
 
 		/// <summary>
 		/// Shows the preview of specified scan data
 		/// </summary>
 		/// <param name="scan">Scan.</param>
-		public void ShowPreviewOf(Scan scan) {
+		public void ShowPreviewOf (Scan scan)
+		{
 			this.currentScan = scan;
+			imageScale = 1.0;
 
-			for (int i = 0; i < tabs.Length; i++) {
-				if (tabs [i].Content != null) {
-					Image image = ((ScanView)tabs [i].Content).Image;
-					if (image != null) {
-						image.Dispose ();
-						((ScanView)tabs [i].Content).Image = null;
-					}
-				}
-			}
-            ScanType type = (ScanType)this.CurrentTabIndex;
-            Thread imageLoaderThread = new Thread(() => LoadPreview(scan, type));
-			imageLoaderThread.Start ();
+			scanView = new ScanView (scan, (ScanType)notebook.CurrentTabIndex);
+			tab.Content = scanView;
+			scanView.RegisterImageLoadedCallback (new MyCallBack (ImageLoadCallBack));
+			scanView.MouseScrolled += delegate(object sender, MouseScrolledEventArgs e) {
+				OnPreviewZoom (e);
+			};
+
+			ScanType type = (ScanType)notebook.CurrentTabIndex;
+			ShowPreview (type);
 		}
 
+		private void ImageLoadCallBack (ScanType type)
+		{
+			if (imageScale == 1.0) {
+				ResizeImageToFit ();
+			}
+		}
 
 		/// <summary>
-		/// Loads an specific preview into appropriate tab.
+		/// Shows an specific preview into appropriate tab.
 		/// </summary>
 		/// <param name="type">Type.</param>
-		private void LoadPreview(Scan scan, ScanType type) {
-            lock (this.lock_image_loading)
-            {
-                if (scan == null)
-                {
-                    return;
-                }
+		private void ShowPreview (ScanType type)
+		{
 
-                ScrollView tab = tabs[(int)type];
-                ScanView scanView = (ScanView)tab.Content;
-
-                if (scanView.Image != null)
-                {
-                    return;
-                }
-
-                MemoryStream memoryStream = new MemoryStream();
-                System.Drawing.Bitmap bmp = scan.GetAsBitmap(type);
-                if (bmp == null)
-                {
-                    Console.WriteLine("bmp == null " + (int)type);
-                    // TODO raise error
-                    return;
-                }
-                bmp.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Tiff);
-
-                memoryStream.Position = 0;
-
-                Application.Invoke(delegate()
-                {
-                    scanView.Image = Image.FromStream(memoryStream);
-                    scanView.Scale(imageScale);
-
-
-                    // resize image to fit window, only on standard zoom!
-                    if (imageScale == 1.0)
-                    {
-                        ResizeImageToFit(tab);
-                    }
-
-                    scanView.MouseScrolled += delegate(object sender, MouseScrolledEventArgs e)
-                    {
-                        OnPreviewZoom(e);
-                    };
-
-                    tab.HorizontalScrollControl.Value = hScroll;
-                    tab.VerticalScrollControl.Value = vScroll;
-                });
-            }
+			if (scanView != null) {
+				scanView.ScanType = type;
+			}
 		}
 
 		/// <summary>
 		/// Initializes all events.
 		/// </summary>
 		/// <param name="view">View.</param>
-		private void InitializeEvents(ScrollView view) {
+		private void InitializeEvents (ScrollView view)
+		{
 
 			view.BoundsChanged += delegate(object sender, EventArgs e) {
-				ResizeImageToFit((ScrollView) sender);
+				ResizeImageToFit ();
 			};
 
-			view.ButtonPressed += delegate(object sender, ButtonEventArgs e)
-			{
-				ScrollView sv = (ScrollView) sender;
-				ScanView scanView = (ScanView) sv.Content;
+			view.ButtonPressed += delegate(object sender, ButtonEventArgs e) {
+				//ScrollView sv = (ScrollView) sender;
+				//ScanView scanView = (ScanView) sv.Content;
 
-				switch(e.Button) {
+				switch (e.Button) {
 				case PointerButton.Left:
 
 					break;
 				case PointerButton.Middle:
-					if(scanView != null) {
-						scanView.Data["pressed"] = true;
-						scanView.Data["pressedPosition"] = e.Position;
+					if (scanView != null) {
+						scanView.Data ["pressed"] = true;
+						scanView.Data ["pressedPosition"] = e.Position;
 					}
 					break;
 				}
 			};
 
-			view.ButtonReleased += delegate(object sender, ButtonEventArgs e)
-			{
-				ScrollView sv = (ScrollView) sender;
-				ScanView scanView = (ScanView) sv.Content;
-
-				switch(e.Button) {
+			view.ButtonReleased += delegate(object sender, ButtonEventArgs e) {
+				switch (e.Button) {
 				case PointerButton.Middle:
-					if(scanView != null) {
-						scanView.Data.Remove("pressed");
+					if (scanView != null) {
+						scanView.Data.Remove ("pressed");
 					}
 					break;
 				}
 			};
 
-			view.MouseExited += delegate(object sender, EventArgs e)
-			{
-				ScrollView sv = (ScrollView) sender;
-				ScanView scanView = (ScanView) sv.Content;
-
-				if(scanView != null) {
-					scanView.Data.Remove("pressed");
+			view.MouseExited += delegate(object sender, EventArgs e) {
+				if (scanView != null) {
+					scanView.Data.Remove ("pressed");
 				}
 			};
 
@@ -173,37 +130,18 @@ namespace bachelorarbeit_implementierung
 				view.MouseMoved += MouseMovedNotGtk;
 			}
 
-			view.HorizontalScrollControl.ValueChanged += delegate(object sender, EventArgs e) {
-				ScrollControl sc = (ScrollControl) sender;
 
-				hScroll = sc.Value;
-				foreach (ScrollView s in tabs) {
-					s.HorizontalScrollControl.Value = sc.Value;
-				}
-			};
-
-			view.VerticalScrollControl.ValueChanged += delegate(object sender, EventArgs e) {
-				ScrollControl sc = (ScrollControl) sender;
-
-				vScroll = sc.Value;
-				foreach (ScrollView s in tabs) {
-					s.VerticalScrollControl.Value = sc.Value;
-				}
-			};
-
-			view.MouseScrolled += delegate(object sender, MouseScrolledEventArgs e)
-			{
-				OnPreviewZoom(e);
+			view.MouseScrolled += delegate(object sender, MouseScrolledEventArgs e) {
+				OnPreviewZoom (e);
 			};
 		}
-
 
 		/// <summary>
 		/// Resize preview image on scrolling
 		/// </summary>
 		/// <param name="sender">Sender.</param>
 		/// <param name="e">Event args</param>
-		private void OnPreviewZoom(MouseScrolledEventArgs e) 
+		private void OnPreviewZoom (MouseScrolledEventArgs e)
 		{
 			if (e.Direction == ScrollDirection.Down) {
 				imageScale *= 0.9;
@@ -211,119 +149,96 @@ namespace bachelorarbeit_implementierung
 				imageScale *= 1.1;
 			}
 
-			for( int i = 0; i < tabs.Length; i++) {
-				if (tabs [i].Content != null) {
-					ScanView scanview = (ScanView)tabs [i].Content;
-					if (scanview != null) {
-						if (e.Direction == ScrollDirection.Down) {
-							scanview.Scale (0.9);
-						} else {
-							scanview.Scale (1.1);
-						}
-					}
+			if (scanView != null) {
+				if (e.Direction == ScrollDirection.Down) {
+					scanView.Scale (0.9);
+				} else {
+					scanView.Scale (1.1);
 				}
 			}
 
 			e.Handled = true;
 		}
 
+		private void ResizeImageToFit ()
+		{
+			ScanView iv = (ScanView)tab.Content;
 
-		private void ResizeImageToFit(ScrollView sv) {
-			ScanView iv = (ScanView) sv.Content;
+			if (iv != null && iv.Image != null) {
 
-			if(iv != null && iv.Image != null) {
-
-				double width = sv.VisibleRect.Width / iv.Image.Size.Width;
-				double height = sv.VisibleRect.Height / iv.Image.Size.Height;
+				double width = tab.VisibleRect.Width / iv.Image.Size.Width;
+				double height = tab.VisibleRect.Height / iv.Image.Size.Height;
 
 				imageScale *= Math.Min (width, height);
-				iv.Scale (Math.Min (width, height));
+				if (scanView != null) {
+					scanView.Scale (Math.Min (width, height));
+				}
 			}
 		}
-
 
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <param name="sender">ScrollView.</param>
 		/// <param name="e">Mouse event args.</param>
-		private void MouseMovedNotGtk (object sender, MouseMovedEventArgs e) {
+		private void MouseMovedNotGtk (object sender, MouseMovedEventArgs e)
+		{
 			e.Handled = false;
 
-			ScrollView sc = (ScrollView)sender;
-			sc.MouseMoved -= MouseMovedNotGtk;
+			tab.MouseMoved -= MouseMovedNotGtk;
 
-			ScanView img = (ScanView)sc.Content;
+			if (scanView != null && scanView.Image != null) {
+				if (scanView.Data.ContainsKey ("pressed") &&
+				    scanView.Data.ContainsKey ("pressedPosition") &&
+				    scanView.Data ["pressedPosition"] != null) {
 
-			if (img != null && img.Image != null) {
-				if (img.Data.ContainsKey ("pressed") &&
-				    img.Data.ContainsKey ("pressedPosition") &&
-				    img.Data ["pressedPosition"] != null) {
+					Point oldPosition = (Point)scanView.Data ["pressedPosition"];
 
-					Point oldPosition = (Point)img.Data ["pressedPosition"];
+					double newScrollX = tab.HorizontalScrollControl.Value + oldPosition.X - e.Position.X;
+					double newScrollY = tab.VerticalScrollControl.Value + oldPosition.Y - e.Position.Y;
 
-					double newScrollX = sc.HorizontalScrollControl.Value + oldPosition.X - e.Position.X;
-					double newScrollY = sc.VerticalScrollControl.Value + oldPosition.Y - e.Position.Y;
-
-					foreach(ScrollView s in tabs) {
-						s.HorizontalScrollControl.Value =
-							Math.Min (sc.HorizontalScrollControl.UpperValue, newScrollX);
-						s.VerticalScrollControl.Value =
-							Math.Min (sc.VerticalScrollControl.UpperValue, newScrollY);
-					}
+					tab.HorizontalScrollControl.Value =
+						Math.Min (tab.HorizontalScrollControl.UpperValue, newScrollX);
+					tab.VerticalScrollControl.Value =
+						Math.Min (tab.VerticalScrollControl.UpperValue, newScrollY);
 				}
 			}
 
-			img.Data["pressedPosition"] = e.Position;
-			sc.MouseMoved += MouseMovedNotGtk;
+			scanView.Data ["pressedPosition"] = e.Position;
+			tab.MouseMoved += MouseMovedNotGtk;
 		}
-
 
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <param name="sender">ScrollView</param>
 		/// <param name="e">Mouse event args.</param>
-		private void MouseMovedGtk(object sender, MouseMovedEventArgs e)
+		private void MouseMovedGtk (object sender, MouseMovedEventArgs e)
 		{
 			e.Handled = false;
 
-			ScrollView sc = (ScrollView)sender;
-			sc.MouseMoved -= MouseMovedGtk;
+			tab.MouseMoved -= MouseMovedGtk;
 
-			ScanView img = (ScanView)sc.Content;
+			//ScanView img = (ScanView)sc.Content;
 
-			if (img != null && img.Image != null) {
-				if (img.Data.ContainsKey ("pressed") &&
-					img.Data.ContainsKey ("pressedPosition") &&
-				    img.Data ["pressedPosition"] != null) {
+			if (scanView != null && scanView.Image != null) {
+				if (scanView.Data.ContainsKey ("pressed") &&
+				    scanView.Data.ContainsKey ("pressedPosition") &&
+				    scanView.Data ["pressedPosition"] != null) {
 
-					Point oldPosition = (Point)img.Data ["pressedPosition"];
+					Point oldPosition = (Point)scanView.Data ["pressedPosition"];
 
-					double newScrollX = sc.HorizontalScrollControl.Value + oldPosition.X - e.Position.X;
-					double newScrollY = sc.VerticalScrollControl.Value + oldPosition.Y - e.Position.Y;
+					double newScrollX = tab.HorizontalScrollControl.Value + oldPosition.X - e.Position.X;
+					double newScrollY = tab.VerticalScrollControl.Value + oldPosition.Y - e.Position.Y;
 
-					sc.HorizontalScrollControl.Value =
-						Math.Min (sc.HorizontalScrollControl.UpperValue - sc.VisibleRect.Width, newScrollX);
-					sc.VerticalScrollControl.Value =
-						Math.Min (sc.VerticalScrollControl.UpperValue - sc.VisibleRect.Height, newScrollY);
+					tab.HorizontalScrollControl.Value =
+						Math.Min (tab.HorizontalScrollControl.UpperValue - tab.VisibleRect.Width, newScrollX);
+					tab.VerticalScrollControl.Value =
+						Math.Min (tab.VerticalScrollControl.UpperValue - tab.VisibleRect.Height, newScrollY);
 				}
 			}
 
-			sc.MouseMoved += MouseMovedGtk;
-		}
-
-
-		/// <summary>
-		/// Raises the current tab changed event.
-		/// </summary>
-		/// <param name="e">Event args</param>
-		protected override void OnCurrentTabChanged (EventArgs e) {
-			base.OnCurrentTabChanged (e);
-
-            ScanType type = (ScanType)this.CurrentTabIndex;
-			imageLoaderThread = new Thread (() => LoadPreview (currentScan, type));
-			imageLoaderThread.Start ();
+			tab.MouseMoved += MouseMovedGtk;
 		}
 	}
 }
--- ui/widget/ScanView.cs
+++ ui/widget/ScanView.cs
@@ -2,21 +2,31 @@ using System;
 using Xwt;
 using System.Collections.Generic;
 using Xwt.Drawing;
+using System.Threading;
+using System.IO;
 
 namespace bachelorarbeit_implementierung
 {
 	public class ScanView : Table
 	{
+		private object lock_image_loading = new object ();
+		Preview.MyCallBack imageLoadedCallback = null;
+
 		public Dictionary<string, object> Data = new Dictionary<string, object> ();
 
 		private double scale = 1.0f;
 		private ImageView image;
 		private ImageView mask;
+		private Scan scan;
+		private ScanType currentShownType;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="bachelorarbeit_implementierung.ScanView"/> class.
 		/// </summary>
-		public ScanView() {
+		public ScanView (Scan scan, ScanType scanType)
+		{
+			this.scan = scan;
+
 			image = new ImageView ();
 			mask = new ImageView ();
 
@@ -25,12 +35,23 @@ namespace bachelorarbeit_implementierung
 
 			this.Add (image, 0, 0);
 			this.Add (mask, 0, 0);
+
+			ScanType = scanType;
 		}
 
-		public void Scale(double scale)
+		/// <summary>
+		/// Scale scan and mask at once.
+		/// </summary>
+		/// <param name="scale">Scale factor.</param>
+		public void Scale (double scale)
 		{
 			this.scale *= scale;
 
+			ScaleIntern (scale);
+		}
+
+		private void ScaleIntern (double scale)
+		{
 			if (image.Image != null) {
 				image.Image = image.Image.Scale (scale);
 			}
@@ -39,6 +60,46 @@ namespace bachelorarbeit_implementierung
 			}
 		}
 
+		private void ShowType (ScanType type)
+		{
+			currentShownType = type;
+
+			lock (this.lock_image_loading) {
+				if (scan == null) {
+					return;
+				}
+
+				//if (Image != null)
+				//{
+				//	return;
+				//}
+
+				MemoryStream memoryStream = new MemoryStream ();
+				System.Drawing.Bitmap bmp = scan.GetAsBitmap (type);
+				if (bmp == null) {
+					Console.WriteLine ("bmp == null " + (int)type);
+					// TODO raise error
+					return;
+				}
+				bmp.Save (memoryStream, System.Drawing.Imaging.ImageFormat.Tiff);
+
+				memoryStream.Position = 0;
+
+				Application.Invoke (delegate() {
+					image.Image = Image.FromStream (memoryStream);
+					ScaleIntern (scale);
+
+					if (imageLoadedCallback != null) {
+						imageLoadedCallback (type);
+					}
+				});
+			}
+		}
+
+		public void RegisterImageLoadedCallback (bachelorarbeit_implementierung.Preview.MyCallBack cb)
+		{
+			this.imageLoadedCallback = cb;
+		}
 
 		/// <summary>
 		/// Gets or sets the scan image to show.
@@ -49,28 +110,28 @@ namespace bachelorarbeit_implementierung
 				return image.Image;
 			}
 
-			set {
-				image.Image = value;
-				if (value != null) {
-					ImageBuilder ib = new ImageBuilder (30, 30);
-					ib.Context.Arc (15, 15, 15, 0, 360);
-					ib.Context.SetColor (Color.FromBytes(255, 0, 100));
-					ib.Context.Fill ();
-					ib.Context.SetColor (Colors.DarkKhaki);
-					ib.Context.Rectangle (0, 0, 5, 5);
-					ib.Context.Fill ();
-					var img = ib.ToVectorImage ();
+			//set {
+			//	image.Image = value;
 
-					mask.Image = img;
-
-					//ImageBuilder ib = new ImageBuilder (value.Width, value.Height);
-					//ib.Context.SetColor (Color.FromBytes(255, 0, 100));
-					//ib.Context.Rectangle (new Rectangle (Point.Zero, new Size (100, 100)));
-					//mask.Image = ib.ToBitmap ();
-				}
-			}
+//				if (value != null) {
+//					ImageBuilder ib = new ImageBuilder (value.Width, value.Height);
+//					ib.Context.Arc (150, 15, 15, 0, 360);
+//					ib.Context.SetColor (Color.FromBytes(255, 0, 100));
+//					ib.Context.Fill ();
+//					ib.Context.SetColor (Colors.Aqua);
+//					ib.Context.Rectangle (0, 0, 50, 50);
+//					ib.Context.Fill ();
+//					var img = ib.ToVectorImage ();
+//
+//					mask.Image = img;
+//
+//					//ImageBuilder ib = new ImageBuilder (value.Width, value.Height);
+//					//ib.Context.SetColor (Color.FromBytes(255, 0, 100));
+//					//ib.Context.Rectangle (new Rectangle (Point.Zero, new Size (100, 100)));
+//					//mask.Image = ib.ToBitmap ();
+//				}
+				//}
 		}
-
 
 		/// <summary>
 		/// Gets or sets the mask.
@@ -83,6 +144,16 @@ namespace bachelorarbeit_implementierung
 
 			set {
 				mask.Image = value;
+			}
+		}
+
+		public ScanType ScanType {
+			get {
+				return currentShownType;
+			}
+			set {
+				Thread imageLoaderThread = new Thread (() => ShowType (value));
+				imageLoaderThread.Start ();
 			}
 		}
 	}
