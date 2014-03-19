e52c3442fbffdf04610610e14b41802568c75f21
Jens Dieskau <jens@dieskau.pm>
2014-03-19 18:37:57 +0000

    Graph structure

---
+++ Data/GraphNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace baimp
+{
+	public class GraphNode<T> : Node<T>
+	{
+		private List<int> costs;
+
+		public GraphNode() : base() { }
+		public GraphNode(T value) : base(value) { }
+		public GraphNode(T value, NodeList<T> neighbors) : base(value, neighbors) { }
+
+		new public NodeList<T> Neighbors
+		{
+			get
+			{
+				if (base.Neighbors == null)
+					base.Neighbors = new NodeList<T>();
+
+				return base.Neighbors;
+			}            
+		}
+
+		public List<int> Costs
+		{
+			get
+			{
+				if (costs == null)
+					costs = new List<int>();
+
+				return costs;
+			}
+		}
+	}
+}
+
