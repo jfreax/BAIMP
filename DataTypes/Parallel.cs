318bb4790a54c32af9082d8ad64fa0e44dd72eff
Jens Dieskau <jens@dieskau.pm>
2014-03-25 15:04:04 +0000

    Sequential and parallel data type wrapper

---
+++ DataTypes/Parallel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace baimp
+{
+	public class Parallel<T> where T : IType
+	{
+		public Parallel()
+		{
+		}
+	}
+}
+
---
+++ DataTypes/Sequential.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace baimp
+{
+	public class Sequential<T> where T : IType
+	{
+		public Sequential()
+		{
+		}
+	}
+}
+
