8405c41e63b7eeb6214337e246bc3ea0353ac79e
Jens Dieskau <jens@dieskau.pm>
2014-03-25 16:35:50 +0000

    Working scan data loading

--- Algorithm/Input/ProjectFiles.cs
+++ Algorithm/Input/ProjectFiles.cs
@@ -6,6 +6,8 @@ namespace baimp
 {
 	public class ProjectFiles : BaseAlgorithm
 	{
+		int position = 0;
+
 		public ProjectFiles(PipelineNode parent) : base(parent)
 		{
 			output.Add(new Compatible(
@@ -29,16 +31,26 @@ namespace baimp
 
 		public override IType[] Run(Dictionary<RequestType, object> requestedData, IType[] inputArgs)
 		{
-			IType[] output = new IType[3];
 			ScanCollection scans = requestedData[RequestType.ScanCollection] as ScanCollection;
 
+			int i = 0;
 			foreach (string key in scans.Keys) {
 				foreach (Scan scan in scans[key]) {
+					if (i == position) {
+						Console.WriteLine(scan.Name + " for position " + position);
+						IType[] output = new IType[3];
+						output[0] = new TBitmap(scan.GetAsBitmap(ScanType.Intensity));
+						output[1] = new TBitmap(scan.GetAsBitmap(ScanType.Topography));
+						output[2] = new TBitmap(scan.GetAsBitmap(ScanType.Color));
+
+						position++;
+						return output;
+					}
+					i++;
 				}
 			}
 
-
-			return output;
+			return null;
 		}
 
 		public override AlgorithmType AlgorithmType {
--- Processing/Process.cs
+++ Processing/Process.cs
@@ -16,6 +16,7 @@ namespace baimp
 			this.project = project;
 			this.startNode = startNode;
 
+			startNode.algorithm.requestedData.Clear();
 			foreach (RequestType request in startNode.algorithm.Request) {
 				switch (request) {
 				case RequestType.Filenames:
@@ -36,35 +37,64 @@ namespace baimp
 			OnTaskCompleteDelegate callback = new OnTaskCompleteDelegate(OnFinish);
 			ThreadPool.QueueUserWorkItem(o => {
 				IType[] output = startNode.algorithm.Run(startNode.algorithm.requestedData, input);
-
-				callback(output);
+				if( output != null) { // null means, there is no more data
+					callback(output);
+				}
 			});
 		}
 
 		/// <summary>
 		/// Callback function called when algorithm finished
 		/// </summary>
-		/// <param name="output">Output of algorithm.</param>
-		private void OnFinish(IType[] output)
+		/// <param name="result">Output of algorithm.</param>
+		private void OnFinish(IType[] result)
 		{
 			List<Compatible> compatibleOutput = startNode.algorithm.Output;
 
-			if (output.Length != compatibleOutput.Count) {
+			if (result.Length != compatibleOutput.Count) {
 				throw new ArgumentOutOfRangeException(); // TODO throw a proper exception
 			}
 
-			int offsetIndex = startNode.algorithm.Input.Count;
-			for (int i = 0; i < output.Length; i++) {
-				if (!compatibleOutput[i].Type.IsAssignableFrom(output[i].GetType())) {
-					throw new TypeAccessException(); // TODO throw a proper exception
+			// is one of these outputs sequential, then start next run
+			foreach (Compatible comp in compatibleOutput) {
+				if (comp.Type.IsGenericType &&
+				    comp.Type.GetGenericTypeDefinition().IsEquivalentTo(typeof(Sequential<>))) {
+					if (startNode.IsReady()) {
+						Process newProcess = new Process(project, startNode);
+						newProcess.Start(startNode.DequeueInput());
+						break;
+					}
 				}
+			}
 
+
+			int offsetIndex = startNode.algorithm.Input.Count;
+			for (int i = 0; i < result.Length; i++) {
+//				if (!compatibleOutput[i].Type.IsAssignableFrom(output[i].GetType())) {
+//					throw new TypeAccessException(); // TODO throw a proper exception
+//				}
+
+				// enqueue new data
 				foreach (MarkerEdge edge in startNode.MNodes[offsetIndex+i].Edges) {
 					MarkerNode targetNode = edge.to as MarkerNode;
-					targetNode.inputData.Enqueue(output[i]);
 
+					bool targetIsParallel = false;
+					Type tmpType = startNode.MNodes[offsetIndex + i].compatible.Type;
+					if (tmpType.IsGenericType &&
+					    tmpType.GetGenericTypeDefinition().IsEquivalentTo(typeof(Parallel<>))) {
+						targetIsParallel = true;
+					}
+
+					if (result[i].GetType().IsArray && !targetIsParallel) {
+						foreach (IType t in (result[i] as IType[])) {
+							targetNode.inputData.Enqueue(t);
+						}
+					} else {
+						targetNode.inputData.Enqueue(result[i]);
+					}
+
+					// start next node
 					if (targetNode.parent.IsReady()) {
-						// TODO queue the start
 						Process newProcess = new Process(project, targetNode.parent);
 						newProcess.Start(targetNode.parent.DequeueInput());
 					}
