750f3a57e4377a6839f559ce0219791d14dfcfbf
Jens Dieskau <jens@dieskau.pm>
2014-03-28 20:44:30 +0000

    Metadata as xml attributes

--- Data/Metadata.cs
+++ Data/Metadata.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Xml.Serialization;
 
 namespace baimp
 {
 	public class Metadata
 	{
+		[XmlAttribute]
 		public string key = "";
+
+		[XmlAttribute]
 		public string value = "";
 
 		public Metadata()
