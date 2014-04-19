14b7589f2585c508cd0a6f8c4553198be61792f7
Jens Dieskau <jens@dieskau.pm>
2014-04-19 23:08:35 +0000

    Fix null pointer

--- Processing/Pipeline/MarkerNode.cs
+++ Processing/Pipeline/MarkerNode.cs
@@ -98,6 +98,7 @@ namespace Baimp
 					// draw output size on end nodes (= number of features
 					if (parent.results != null &&
 						parent.results.Count != 0 &&
+						parent.results[0].Item1 != null &&
 						parent.results[0].Item1.Length-1 >= Position) {
 
 						textLayout.Text = parent.results.Count.ToString();
