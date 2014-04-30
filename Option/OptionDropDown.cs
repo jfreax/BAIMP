65a8ae663f725dff845f6332d6c328bf177cf5dd
Jens Dieskau <jens@dieskau.pm>
2014-04-30 00:20:04 +0000

    Fix default value in dropdown option

--- Option/OptionDropDown.cs
+++ Option/OptionDropDown.cs
@@ -19,6 +19,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
+using System.Linq;
 using System.Xml.Serialization;
 using Xwt;
 
@@ -93,6 +94,9 @@ namespace Baimp
 			}
 			set {
 				values = value;
+				if (!values.Contains(val)) {
+					val = (string) values[0];
+				}
 			}
 		}
 	}
